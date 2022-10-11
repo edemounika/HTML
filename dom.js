0adab0737c94f2f39d0f0ce4ccf3134ade096010
Ede Mounika <edemounika99@gmail.com>
2022-10-11 20:43:54 +0530

    html concepts

--- dom.js
+++ dom.js
@@ -1,2 +1,8 @@
-const p = document.getElementById('message');
-console.log(p);
+//const p = document.getElementById('message');
+//console.log(p);
+let menu = document.getElementById('menu');
+let items = menu.getElementsByClassName('item');
+
+let data = [].map.call(items, item => item.textContent);
+
+console.log(data);
